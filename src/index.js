866cecd9b3a2c31daa66951efc5b4bc0f89e8c12
k-okina <okinakahiro@gmail.com>
2018-07-12 13:38:12 +0900

    [add] 改造して遊んでみた

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Object.values(document.getElementsByClassName('root')).forEach(element => {
+  console.log(element);
+  // props.childrenがobjectで1つだけくる
+  console.log(<App />, <div><h1>hello world</h1></div>);
+  // props.childrenがobject[]で2つくる
+  console.log(<App />, <div><h1>hello world</h1><h2>hello again</h2></div>);
+  console.log(typeof <App />);
+  ReactDOM.render(<App />, element);
+});
 registerServiceWorker();
